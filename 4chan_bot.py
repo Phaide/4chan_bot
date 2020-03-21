e56236bb72c650a5c6f4a3db0a85b5305acad9cc
Shawn <33222654+Phaide@users.noreply.github.com>
2020-03-21 15:23:20 +0100

    Add files via upload

---
+++ 4chan_bot.py
@@ -0,0 +1,187 @@
+# coding: utf-8
+
+"""
+
+This script is a 4chan bot
+It is used to parse the website for specific keywords.
+
+Author: Phaide | https://phaide.net/
+Licence: GNU GPL v3
+Repository: https://github.com/Phaide/4chan_bot/
+Build: 27/01/2020
+
+"""
+
+# Import non built-in modules. Requires external installation.
+import requests # pip install requets
+import curses # pip install windows-curses (on Windows of course). Documentation: https://docs.python.org/3/howto/curses.html
+
+# Import build-in modules
+import re, webbrowser
+
+# List of boards to parse.
+boards = ["b"]
+# List of terms to search for. Keep in mind that the script parses HTML, so adapt your terms accordingly.
+terms = ["Facts", "Logic", "Other"]
+
+class b_4chan:
+
+    # 4chan boards subdomain ; boards addresses are built from this address.
+    address = "http://boards.4chan.org"
+
+    def __init__(self, boards, terms):
+        self.boards = boards
+        self.searchingFor = terms
+        self.init_activeThreads()
+
+    def init_activeThreads(self):
+        """
+        Empties the activeThreads dictionnary then recreates it.
+        """
+        self.activeThreads = {}
+        for term in self.searchingFor:
+            self.activeThreads[term] = []
+
+    def main_parser(self):
+        """
+        Main function
+        """
+        for board in self.boards:
+            self.board_parser(board)
+
+    def board_parser(self, board):
+        """
+        Gets the HTML of the board's pages to find threads.
+        """
+        boardAdd = "{}/{}".format(self.address, board)
+        pages = [""] + list(range(2, 10))
+        for page in pages:
+            r = requests.get("{}/{}".format(boardAdd, page))
+            if r.status_code == 200:
+                threads = re.findall(r"id=\"t([0-9]*)\"", r.text)
+                for thread in threads:
+                    self.thread_parser(board, thread)
+
+    def thread_parser(self, board, thread):
+        """
+        Parses each thread's HTML code to find the terms
+        """
+        threadAdd = "{}/{}/thread/{}".format(self.address, board, thread)
+        r = requests.get(threadAdd)
+        if r.status_code == 200:
+            for term in self.searchingFor:
+                count = r.text.lower().count(term.lower())
+                if count > 0:
+                    self.activeThreads[term].append([count, threadAdd])
+
+class Display:
+
+    def __init__(self, menu):
+        self.menu = menu + ["Exit"]
+        self.stdscr = curses.initscr()
+        self.stdscr.keypad(True)
+        curses.noecho()
+        curses.cbreak()
+        curses.curs_set(False)
+        curses.start_color()
+        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+
+    def __del__(self):
+        self.stdscr.keypad(False)
+        curses.echo()
+        curses.nocbreak()
+        curses.endwin()
+
+    def loading_screen(self):
+        """
+        Prints a simple loading screen.
+        """
+        row = "Loading..."
+        self.stdscr.clear()
+        # Get screen dimensions (height and width)
+        h, w = self.stdscr.getmaxyx()
+        # Find the center
+        x = (w // 2) - (len(row) // 2)
+        y = (h // 2)
+        # Print the text
+        self.stdscr.addstr(y, x, row)
+        # Refresh display
+        self.stdscr.refresh()
+
+    def main(self, bot):
+        """
+        Main loop, used to navigate through the menus and execute actions depending on user input.
+        """
+        for index, entry in enumerate(self.menu):
+            try:
+                indexName = next(v for i, v in enumerate(bot.activeThreads) if i == index)
+                try:
+                    self.menu[index] = [len(bot.activeThreads[indexName]), self.menu[index]]
+                except NameError:
+                    continue
+            except:
+                continue
+        currentMenuRowIndex = 0
+        while True:
+            try:
+                # Get the name of the menu entry in the "bot.activeThreads" var, used to access data in this dictionnary
+                currentMenuRowName = next(v for i, v in enumerate(bot.activeThreads) if i == currentMenuRowIndex)
+            except: # Catches StopIteration when selecting menu entry "Exit"
+                pass
+            self.print_list(self.menu, currentMenuRowIndex)
+            key = self.stdscr.getch()
+            if (key == curses.KEY_UP) and (currentMenuRowIndex > 0):
+                currentMenuRowIndex -= 1
+            elif (key == curses.KEY_DOWN) and (currentMenuRowIndex < (len(self.menu) - 1)):
+                currentMenuRowIndex += 1
+            elif (key == ord("u")):
+                self.loading_screen()
+                bot.init_activeThreads()
+                bot.main_parser()
+            elif (key == curses.KEY_RIGHT) and (currentMenuRowIndex == (len(self.menu) - 1)):
+                break
+            elif (key == curses.KEY_RIGHT) and (len(bot.activeThreads[currentMenuRowName]) > 0):
+                currentThreadRowIndex = 0
+                while True:
+                    self.print_list(bot.activeThreads[currentMenuRowName], currentThreadRowIndex)
+                    key = self.stdscr.getch()
+                    if (key == curses.KEY_UP) and (currentThreadRowIndex > 0):
+                        currentThreadRowIndex -= 1
+                    elif (key == curses.KEY_DOWN) and (currentThreadRowIndex < (len(bot.activeThreads[currentMenuRowName]) - 1)):
+                        currentThreadRowIndex += 1
+                    elif (key == curses.KEY_RIGHT):
+                        # Uses the webbrowser module to open a web page.
+                        webbrowser.open(bot.activeThreads[currentMenuRowName][currentThreadRowIndex][1])
+                    elif (key == curses.KEY_LEFT):
+                        break
+
+    def print_list(self, list, currentIndex):
+        """
+        Prints a list's items in the screen center.
+        """
+        self.stdscr.clear()
+        h, w = self.stdscr.getmaxyx()
+        for index, couple in enumerate(list):
+            row = "{} | {}".format(couple[0], couple[1]) if type(couple) == type([]) and (len(couple) == 2) else couple
+            # The two next lines centers the menu entries, vertically and horizontally
+            x = (w // 2) - (len(row) // 2)
+            y = (h // 2) - (len(list) // 2) + index
+            if x in range(0, w) and y in range(0, h):
+                if index == currentIndex:
+                    self.stdscr.addstr(y, x, row, curses.color_pair(1))
+                else:
+                    self.stdscr.addstr(y, x, row)
+            else:
+                self.stdscr.addstr(0, 0, "Too many results to display !\nIncrease the window size and/or dezoom")
+        self.stdscr.refresh()
+
+if __name__ == "__main__":
+    bot = b_4chan(
+        boards,
+        terms
+    )
+    display = Display(bot.searchingFor)
+    display.loading_screen()
+    bot.main_parser()
+    display.main(bot)
+    del bot, display
---
+++ README.md
@@ -0,0 +1,59 @@
+# 4chan_bot
+A bot parsing 4chan for specific keywords.
+
+## Issue
+Sometimes, you don't want to spend a whole lot of time going through a board to find something you're interested in.<br />
+Life would be easier if something could do it for you, right ?<br />
+
+## Solution
+Well you're in the right place !<br />
+This script, written in Python 3, parses through your favorite boards to find specific keywords, and filter only the relevant threads !<br />
+
+## Future work
+A more precise system could be implemented to filter by relevancy (the more the word is used in the thread, the more relevant it is).<br />
+I also intended to add an image recognition system, probably based on Google's API (if there is any, I didn't look into it atm).<br />
+
+
+## Usage
+First off, you'll need to install the modules "requests" and "curses", via pip for instance.
+```
+pip install requests
+```
+For curses, on Windows:
+```
+pip install wcurses
+```
+And I believe it is installed by default on Linux distributions (to check !). Otherwise, it should be 
+```
+pip install curses
+```
+You should then modify the two lists "boards" and "terms" in the source to fit your needs.<br />
+Finally, use the following command (Python 3) to launch it
+```
+python 4chan_bot.py
+```
+When launched, it will load for a while, collecting the info from 4chan.<br />
+
+Once it is done, you will have a list of the terms you are looking for.<br />
+Use the directionnal arrows to navigate :<br />
+Up : select the previous entry<br />
+Down : select the next entry<br />
+Right : go to the next menu (if any is available) or open a link when you're in the thread menu<br />
+Left : go to the previous menu<br />
+
+On the main menu, you can press the right arrow on the "Exit" entry to quit the program.<br />
+Finally, if you want to update the data, press "u" when on the main menu.<br />
+
+## Contributing
+If you want to contribute, please fork this repo and/or send pull requests. Thank you.<br />
+
+## Supporting
+If you want to support me, you can send some kind messages via my website (https://phaide.net/contact)<br />
+
+And perhaps, consider making a donation<br />
+
+    BTC: 178oEM3sUYtHVYVt2jbHv4HNjy2nfu1iiT
+    ETH: 0x4f3290b22012f0d01900a87e4475c01a7f95ee93
+
+With love,<br />
+Phaide.
